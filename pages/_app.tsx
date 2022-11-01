d3fa2c2092ebd3ca5add22b57e639647aac40b13
KazooTTT <wangyijin1999@qq.com>
2022-11-01 19:43:38 +0800

    feat:官网demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
-/*
- * @Author: KazooTTT wangyijin1999@qq.com
- * @Date: 2022-10-29 14:59:04
- * @LastEditors: KazooTTT wangyijin1999@qq.com
- * @LastEditTime: 2022-10-31 23:34:45
- * @FilePath: /nextjs-portal/pages/_app.tsx
- * @Description:
- */
-// .pages/_app.tsx
+// ./pages/_app.tsx
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps, AppContext } from "next/app";
+import App from "next/app";
 import { Layout, ILayoutProps } from "@/components/layout";
+import code from "@/public/code.png";
 
 const MyApp = (data: AppProps & ILayoutProps) => {
   const { Component, pageProps, navbarData, footerData } = data;
@@ -20,4 +14,63 @@ const MyApp = (data: AppProps & ILayoutProps) => {
     </Layout>
   );
 };
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  const pageProps = await App.getInitialProps(context);
+
+  return {
+    ...pageProps,
+    navbarData: {},
+    footerData: {
+      title: "Demo",
+      linkList: [
+        {
+          title: "技术栈",
+          list: [
+            {
+              label: "react",
+            },
+            {
+              label: "typescript",
+            },
+            {
+              label: "ssr",
+            },
+            {
+              label: "nodejs",
+            },
+          ],
+        },
+        {
+          title: "了解更多",
+          list: [
+            {
+              label: "掘金",
+              link: "https://juejin.cn/user/2714061017452557",
+            },
+            {
+              label: "知乎",
+              link: "https://www.zhihu.com/people/zmAboutFront",
+            },
+            {
+              label: "csdn",
+            },
+          ],
+        },
+        {
+          title: "联系我",
+          list: [{ label: "微信" }, { label: "QQ" }],
+        },
+      ],
+      qrCode: {
+        image: code,
+        text: "祯民讲前端微信公众号",
+      },
+      copyRight: "Copyright © 2022 xxx. 保留所有权利",
+      siteNumber: "粤ICP备XXXXXXXX号-X",
+      publicNumber: "粤公网安备 xxxxxxxxxxxxxx号",
+    },
+  };
+};
+
 export default MyApp;
