d6f4cf24df8615cfe8e7bd6245a020cc74892a3c
KazooTTT <wangyijin1999@qq.com>
2022-11-03 20:54:33 +0800

    feat: 服务端获取layout数据

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * @Author: KazooTTT wangyijin1999@qq.com
  * @Date: 2022-10-29 14:59:04
  * @LastEditors: KazooTTT wangyijin1999@qq.com
- * @LastEditTime: 2022-10-29 15:12:06
+ * @LastEditTime: 2022-11-03 20:39:52
  * @FilePath: /nextjs-portal/next.config.js
  * @Description:
  */
@@ -17,7 +17,10 @@ const nextConfig = {
       ...config.resolve.alias,
       "@": path.resolve(__dirname),
     };
-    return config;  
+    return config;
+  },
+  images: {
+    domains: ["127.0.0.1", "localhost"],
   },
 };
 
---
+++ utils/index.ts
@@ -0,0 +1,3 @@
+// ./utils/index.ts
+export const LOCALDOMAIN = "http://localhost:3000";
+export const CMSDOMAIN = "http://localhost:1337";
